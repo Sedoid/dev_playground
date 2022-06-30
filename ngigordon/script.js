54f3028b9968d4923823843b83baffb94cbe5f9f
ngigordon <ngigordon4@gmail.com>
2022-06-30 12:17:50 +0100

    #5

--- ngigordon/script.js
+++ ngigordon/script.js
@@ -1,4 +1,16 @@
 console.log('hello-world');
-var start_piont = 5;
+var start_point = 5;
 var item_container = document.getElementById("target");
-item_container.textContent = ` helloworld` ;
+
+
+// var countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
+// var myFunction = setInterval(() => {
+//     var now = new Date().getTime();
+//     var distance = countDownDate-now;
+//     var minutes = Math.floor((distance % (1000 * 60)) / 1000)
+//     item_container.innerHTML =minutes;
+//     if (distance < 0) {
+//         clearInterval(myFunction);
+//         document.getElementById("demo").innerHTML = "EXPIRED";
+//       }
+// }, 1000);
